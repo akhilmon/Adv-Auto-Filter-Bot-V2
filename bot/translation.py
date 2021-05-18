9513c41d7512ac5509274832a9fb471c1a43892b
akhilmon <84173526+akhilmon@users.noreply.github.com>
2021-05-18 06:50:51 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# (c) @AlbertEinsteinTG
+# (c) @FILMWORLDOFFICIA
 
 class Translation(object):
     
     START_TEXT = """<b>Hey {}!!</b>
 <i>Am Just A Advance Auto Filter Bot....😉
 
-Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
+ഈ ബോട്ട് തികച്ചും @FILM_WORL ഗ്രൂപ്പിലേക്ക് ആണ്. ഞാൻ ആണ് ആ ഗ്രൂപ്പിൽ സിനിമകൾ നൽകുന്നത്😁 
 
 For More Details Click Help Button Below..
-@CrazyBotsz
+@FILM_WORL
 </i>"""    
     
     HELP_TEXT = """
@@ -48,11 +48,11 @@ For More Details Click Help Button Below..
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/@FILMWORLDOFFICIA">@FILMWORLDOFFICIA</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
+<b>➥ Source Code</b> : <i><a href="https://github.com/@FILMWORLDOFFICIA/Adv-Auto-Filter-Bot">Click Me</a></i>
 """
